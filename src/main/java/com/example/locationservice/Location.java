ee903b25ff4ab638a506b0fbafd2bf36a5ffe778
l0r5 <lars-braitinger@web.de>
2020-08-16 20:09:57 +0200

    Add lombok to the project and adapt app

--- src/main/java/com/example/locationservice/Location.java
+++ src/main/java/com/example/locationservice/Location.java
@@ -1,27 +1,11 @@
 package com.example.locationservice;
 
-public class Location {
+import lombok.Data;
 
+
+@Data
+public class Location {
     private final long id;
     private final int uuid;
     private final String location;
-
-
-    public Location(long id, int uuid, String location) {
-        this.id = id;
-        this.uuid = uuid;
-        this.location = location;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public long getUuid() {
-        return uuid;
-    }
-
-    public String getLocation() {
-        return location;
-    }
 }
